9500fcde6f94906b16203db415e73f7997ec5c95
Maikon Canuto <maikon.pedrosa@altran-ais.com.br>
2017-08-25 22:00:54 -0300

    Project spring boot

---
+++ src/main/java/com/mk/mkspringboot/MkspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.mk.mkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkspringbootApplication.class, args);
+	}
+}
