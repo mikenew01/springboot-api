61b8083ff76abb803e1749e23a90d6cfe259252f
Maikon Canuto <maikon.pedrosa@altran-ais.com.br>
2017-08-26 16:31:36 -0300

    Configurando banco h2 na aplicação

--- src/main/java/com/mk/mkspringboot/start/MkSpringbootApplication.java
+++ src/main/java/com/mk/mkspringboot/start/MkSpringbootApplication.java
@@ -3,15 +3,17 @@ package com.mk.mkspringboot.start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan
 @EnableAutoConfiguration
 @EnableConfigurationProperties
-@Component
+@ComponentScan(basePackages = "com.mk.mkspringboot")
+@EnableJpaRepositories("com.mk.mkspringboot.domain.repository")
+@EntityScan("com.mk.mkspringboot.domain.models")
 public class MkSpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MkSpringbootApplication.class, args);
